45bf0df84b0600cbe6f3f7389fdca4852da1a7a4
NotMarra <marra313cz@gmail.com>
2025-03-14 15:58:40 +0100

    cleanup

--- src/routes/login/schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
- 
-export const formSchema = z.object({
- username: z.string().min(2, { message: "Username must be longer than 2 characters"}).max(50),
- password: z.string(),
-});
- 
-export type FormSchema = typeof formSchema;
