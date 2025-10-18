0418d3cb551fd2bf7539d5db279901e44dfffcbe
m4rt1n99 <144900320+m4rt1n99@users.noreply.github.com>
2025-10-18 20:56:24 +0200

    chore: convert theme to shadcn theme

--- src/routes/account/+layout.svelte
+++ src/routes/account/+layout.svelte
@@ -38,7 +38,7 @@
 <div class="fixed bottom-0 w-full border-t-1 grid grid-rows-1 grid-cols-4 gap-2 p-1">
       {#each links as link (link)}
         {@const Icon = link.icon}
-        <a href={link.href} class={['btn flex-col items-center flex justify-center p-2 rounded-lg gap-1 text-white', page.url.pathname === link.href && "bg-blue-500"]} style="filter: brightness(100%)">
+        <a href={link.href} class={['btn flex-col items-center flex justify-center p-2 rounded-lg gap-1 text-white', page.url.pathname === link.href && "bg-primary-600"]} style="filter: brightness(100%)">
           <Icon class="size-5" />
           <span class="text-[10px]">{link.label}</span>
         </a>
