6a1edc1827ce9272f36f40658a29d68f814a9add
m4rt1n99 <144900320+m4rt1n99@users.noreply.github.com>
2025-10-23 22:01:33 +0200

    fix(frontend): fix nav highlighting on nested routes

--- src/routes/account/+layout.svelte
+++ src/routes/account/+layout.svelte
@@ -2,7 +2,7 @@
   import { ModeWatcher } from "mode-watcher";
   import { User } from "lucide-svelte";
   import { Star } from "lucide-svelte";
-  import { Banknote } from "lucide-svelte";
+  import { Boxes } from "lucide-svelte";
   import { Settings } from "lucide-svelte";
 
 	import { page } from '$app/state';
@@ -24,8 +24,8 @@
 
   const links = [
     { label: 'Account', href: '/account', icon: User },
+    { label: 'Products', href: '/account/products', icon: Boxes },
     { label: 'Premium', href: '/account/premium', icon: Star },
-    { label: 'Billing', href: '/account/billing', icon: Banknote },
     { label: 'Settings', href: '/account/settings', icon: Settings },
   ];
 </script>
@@ -39,7 +39,9 @@
 <div class="fixed bottom-0 w-full border-t-1 grid grid-rows-1 grid-cols-4 gap-2 p-1">
       {#each links as link (link)}
         {@const Icon = link.icon}
-        <a href={link.href} class={['btn flex-col items-center flex justify-center p-2 rounded-lg gap-1 text-white', page.url.pathname === link.href && "bg-primary-600"]} style="filter: brightness(100%)">
+        <a href={link.href} class={['btn flex-col items-center flex justify-center p-2 rounded-lg gap-1 text-white', 
+        (page.url.pathname === link.href || page.url.pathname.split("/").length >= 2 && page.url.pathname.includes(link.href.split("/")[2]))
+         && "bg-primary-600"]} style="filter: brightness(100%)">
           <Icon class="size-5" />
           <span class="text-[10px]">{link.label}</span>
         </a>
